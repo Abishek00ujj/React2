402f70bd9d5be194d82c60d057ce5ba66d6b5b36
Subiksha S <arunasaravanan41@gmail.com>
2024-09-23 11:20:22 +0530

    Update App.jsx

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -8,9 +8,9 @@ const App = () => {
           <form className="w-[80%] h-[80%] flex flex-col justify-center items-center gap-4">
             <input type="text" name="" id="" placeholder="Name" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" />
             <input type="text" name="" id="" placeholder="email" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" />
-            <input type="number" name="" id="" placeholder="phone" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" />
-            <input type="password" name="" id="" placeholder="password" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" />
-            <button type="submit" className="w-full bg-purple-600 text-white p-3"> <Loader  className="animate-spin"/>  Register</button>
+            <input type="Number" name="" id="" placeholder="phone" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" />
+            <input type="Password" name="" id="" placeholder="password" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" />
+            <button type="Submit" className="w-full bg-purple-600 text-white p-3"> <Loader  className="animate-spin"/>  Register</button>
           </form>
         </div>
       </div>
