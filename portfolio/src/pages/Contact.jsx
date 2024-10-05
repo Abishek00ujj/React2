a48cbe5d8a3b4e060b1152458ba4e9fc6d8dbb1b
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-10-05 12:07:04 +0530

    ss

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
   const [visible, setvisible] = useState(false)
   return (
     <>
- 
+     
       <div className="h-[80vh] w-screen flex justify-center items-center -z-10">
         <button  className="bg-red-500 p-3 text-white w-32 rounded-sm"> Delete </button>
       </div>
